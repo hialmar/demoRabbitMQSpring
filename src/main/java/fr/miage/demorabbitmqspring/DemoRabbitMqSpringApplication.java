05f6f0714cce53e220f1f9414d8727007564421b
Patrice Torguet <patrice.torguet@irit.fr>
2023-06-07 14:54:27 +0200

    Initial commit

---
+++ src/main/java/fr/miage/demorabbitmqspring/DemoRabbitMqSpringApplication.java
@@ -0,0 +1,13 @@
+package fr.miage.demorabbitmqspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoRabbitMqSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoRabbitMqSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/miage/demorabbitmqspring/DemoRabbitMqSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.miage.demorabbitmqspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoRabbitMqSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
